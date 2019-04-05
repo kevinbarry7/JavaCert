0207ac290a6e79cebebd5debdbba2ab9bfae64c4
Kevin Barry <kevn.barry@gmail.com>
2019-04-05 14:46:00 -0400

    Commit 040519

---
+++ Basics/Person.java
@@ -0,0 +1,8 @@
+//package Basics;
+public class Person {
+	public static void main(String[] args) {
+		System.out.println("Today is Friday the 5th");
+		String name = "/* Harry */ Paul";
+		System.out.println(name);
+	}
+}
