d6b4c9a355af7de7357b8652018b46fc1f30ec5a
Kevin Barry <kevn.barry@gmail.com>
2019-05-04 11:08:02 -0400

    Commit 050419

--- Arrays1.java
+++ Arrays1.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 public class Arrays1 {
     public static void main(String[] args) {
         int[] numbers1 = new int[3];
@@ -62,7 +62,94 @@ public class Arrays1 {
         }
         System.out.println("_________________________");
 
+        String [][] rectangle = new String[3][2];
+        rectangle[0][1] = "set";
+        System.out.println(rectangle[0][1]);
+        System.out.println("_________________________");
+
+        int[][] twoD = new int[3] [2];
+        for(int i = 0; i < twoD.length; i++){
+            for(int j = 0; j < twoD[i].length; j++)
+                System.out.print(twoD[i][j] + " ");
+            System.out.println();
+        }
+        System.out.println("_________________________");
+
+        int [][] twoDs = new int[4][2];
+        for(int[] inner : twoDs) {
+            for(int num: inner)
+                System.out.print(num + " ");
+            System.out.println();
+        }
+        System.out.println("_________________________");
+
+        ArrayList list1 = new ArrayList(); //old declaration but still valid
+        ArrayList list2 = new ArrayList(10); //old declaration but still valid
+        ArrayList list3 = new ArrayList(list2); //old declaration but still valid
+
+        ArrayList<String> list4 = new ArrayList<String>(); //current preferred declaration
+        ArrayList<String> list5 = new ArrayList<>(); //also a version of the current preferred dec
+
+        ArrayList list = new ArrayList<>();
+        list.add("hawk");
+        list.add(Boolean.TRUE);
+        list.add('1');
+        System.out.println(list);
+        System.out.println("_________________________");
+
+        ArrayList<String> safer = new ArrayList<>();
+
+        //add() // add method
+
+        safer.add("sparrow");
+        //safer.add(Boolean.TRUE); //won't compile since safer is defined as String
+
+        List<String> birds = new ArrayList<>();
+        birds.add("hawk");
+        birds.add(1, "robin");
+        birds.add(0, "blue jay");
+        birds.add(1, "cardinal");
+        System.out.println(birds);
+
+        //remove() // remove method
+
+        birds.add("thrush");
+        birds.add("thrush");
+        System.out.println(birds);
+        birds.remove("thrush");
+        System.out.println(birds.remove("hawk"));
+        System.out.println(birds.remove(1));
+        System.out.println(birds);
+
+        //set()
+        ArrayList<String> bird = new ArrayList<>();
+        bird.add("hawk");
+        System.out.println(bird.size());
+        bird.set(0, "black bird");
+        //bird.set(1, "thrush"); //won't compile, IndexOutOfBoundsException
+        System.out.println(bird);
+
+        System.out.println(bird.isEmpty()); //isEmpty()
+        System.out.println(bird.size()); //size()
+        System.out.println(bird.contains("black bird")); //contains()
+        System.out.println(bird.contains("robin"));
         
+
+        bird.clear();
+        System.out.println(bird.isEmpty()); //isEmpty()
+        System.out.println(bird.size()); //size()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
